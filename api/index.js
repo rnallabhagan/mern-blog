2f93f681dfdaa4474d9e990e6e990ec9bd77e3d9
Nallabhagan <98387138+rnallabhagan@users.noreply.github.com>
2024-02-14 21:53:59 +0530

    connect to the mongo db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log("Mongo DB is connected"))
+	.catch((error) => console.log(error));
+
 const app = express();
 
 app.listen(3000, () => {
